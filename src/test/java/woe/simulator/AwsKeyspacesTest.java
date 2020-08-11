37fd9fb0dda506c589c8abca6b63a5d22a799535
Hugh McKee <mckeeh3@gmail.com>
2020-08-11 10:01:59 -0400

    adjust aws keyspaces config

--- src/test/java/woe/simulator/AwsKeyspacesTest.java
+++ src/test/java/woe/simulator/AwsKeyspacesTest.java
@@ -6,22 +6,22 @@ import org.junit.Ignore;
 import org.junit.Test;
 
 import javax.net.ssl.SSLContext;
-import java.net.InetSocketAddress;
 import java.security.NoSuchAlgorithmException;
-import java.util.Collections;
-import java.util.List;
 
 public class AwsKeyspacesTest {
   @Ignore
   @Test
-  public void t() throws NoSuchAlgorithmException {
-    final List<InetSocketAddress> contactPoints = Collections.singletonList(InetSocketAddress.createUnresolved("cassandra.us-east-1.amazonaws.com", 9142));
+  public void queryTables() throws NoSuchAlgorithmException {
     try (final CqlSession cqlSession = CqlSession.builder()
-             .addContactPoints(contactPoints)
              .withSslContext(SSLContext.getDefault())
              .build()) {
 
-      final ResultSet resultSet = cqlSession.execute("select * from woe_simulator.messages");
+      ResultSet resultSet = cqlSession.execute("select * from woe_simulator.messages");
+      System.out.printf("Rows in messages table %,d%n", resultSet.all().size());
+      resultSet.forEach(System.out::println);
+
+      resultSet = cqlSession.execute("select * from woe_simulator.metadata");
+      System.out.printf("Rows in metedata table %,d%n", resultSet.all().size());
       resultSet.forEach(System.out::println);
     }
   }
