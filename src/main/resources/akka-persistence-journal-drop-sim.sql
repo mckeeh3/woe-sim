2e28ffbf7acf033a3d6de38bd39934315ef3ec43
Hugh McKee <mckeeh3@gmail.com>
2021-05-24 13:58:35 -0400

    update for minikube and kind

---
+++ src/main/resources/akka-persistence-journal-drop-sim.sql
@@ -0,0 +1,4 @@
+
+DROP TABLE IF EXISTS public.woe_twin_event_tag;
+
+DROP TABLE IF EXISTS public.woe_twin_event_journal;
--- src/main/resources/akka-persistence-journal-drop.sql
+++
@@ -1,5 +0,0 @@
-
-
-DROP TABLE IF EXISTS journal;
-
-DROP TABLE IF EXISTS snapshot;
